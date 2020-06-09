40fe6076b3ba4ae9f2a6b503bb44e55050ae074e
netkas <netkas@intellivoid.info>
2020-06-09 18:46:34 -0400

    Added DatabaseException to \SpamProtection\Exceptions

--- src/SpamProtection/SpamProtection.php
+++ src/SpamProtection/SpamProtection.php
@@ -9,6 +9,7 @@
 
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Abstracts' . DIRECTORY_SEPARATOR . 'TelegramChatType.php');
 
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'DatabaseException.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'InvalidSearchMethod.php');
 
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Objects' . DIRECTORY_SEPARATOR . 'TelegramClient' . DIRECTORY_SEPARATOR . 'Chat.php');
