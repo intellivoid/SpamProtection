226abab76cc9cf3b9a8dc6c39d537733e6a7c68c
netkas <netkas@intellivoid.info>
2020-06-09 18:49:26 -0400

    Added public constructor to \SpamProtection\Managers > TelegramClientManager

--- src/SpamProtection/Managers/TelegramClientManager.php
+++ src/SpamProtection/Managers/TelegramClientManager.php
@@ -4,11 +4,25 @@
     namespace SpamProtection\Managers;
 
 
+    use SpamProtection\SpamProtection;
+
     /**
      * Class TelegramClientManager
      * @package SpamProtection\Managers
      */
     class TelegramClientManager
     {
+        /**
+         * @var SpamProtection
+         */
+        private $spamProtection;
 
+        /**
+         * TelegramClientManager constructor.
+         * @param SpamProtection $spamProtection
+         */
+        public function __construct(SpamProtection $spamProtection)
+        {
+            $this->spamProtection = $spamProtection;
+        }
     }
