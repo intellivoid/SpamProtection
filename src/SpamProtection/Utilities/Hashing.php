e2c206a1e80bf1ef7966397957d4e83cd948774a
netkas <netkas@intellivoid.info>
2020-06-09 19:02:59 -0400

    Added statuc method telegramClientPublicID() to \SpamProtection\Utilities > Hashing

--- src/SpamProtection/Utilities/Hashing.php
+++ src/SpamProtection/Utilities/Hashing.php
@@ -6,5 +6,20 @@
 
     class Hashing
     {
+        /**
+         * Creates a unique public telegram client ID
+         *
+         * @param string $chat_id
+         * @param int $user_id
+         * @return string
+         */
+        public static function telegramClientPublicID(string $chat_id, int $user_id): string
+        {
+            $builder = "TEL-";
 
+            $builder .= hash('sha256', $chat_id);
+            $builder .= '-' . hash('crc32', $user_id);
+
+            return $builder;
+        }
     }
