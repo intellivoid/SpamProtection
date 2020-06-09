d27076153236c550d0a7d675273b23c2059a79df
netkas <netkas@intellivoid.info>
2020-06-09 18:58:37 -0400

    Added method updateClient() to \SpamProtection\Managers > TelegramClientManager

--- src/SpamProtection/Managers/TelegramClientManager.php
+++ src/SpamProtection/Managers/TelegramClientManager.php
@@ -95,4 +95,48 @@
                 return TelegramClient::fromArray($Row);
             }
         }
+
+        /**
+         * Updates an existing client in the database
+         *
+         * @param TelegramClient $telegramClient
+         * @return bool
+         * @throws DatabaseException
+         */
+        public function updateClient(TelegramClient $telegramClient): bool
+        {
+            $id = (int)$telegramClient->ID;
+            $available = (int)$telegramClient->Available;
+            $account_id = (int)$telegramClient->AccountID;
+            $user = ZiProto::encode($telegramClient->User->toArray());
+            $user = $this->spamProtection->getDatabase("IVDatabase")->real_escape_string($user);
+            $chat = ZiProto::encode($telegramClient->Chat->toArray());
+            $chat = $this->spamProtection->getDatabase("IVDatabase")->real_escape_string($chat);
+            $session_data = ZiProto::encode($telegramClient->SessionData->toArray());
+            $session_data = $this->spamProtection->getDatabase("IVDatabase")->real_escape_string($session_data);
+            $chat_id = $this->spamProtection->getDatabase("IVDatabase")->real_escape_string($telegramClient->Chat->ID);
+            $user_id = $this->spamProtection->getDatabase("IVDatabase")->real_escape_string($telegramClient->User->ID);
+            $last_activity = (int)time();
+
+            $Query = QueryBuilder::update('telegram_clients', array(
+                'available' => $available,
+                'account_id' => $account_id,
+                'user' => $user,
+                'chat' => $chat,
+                'session_data' => $session_data,
+                'chat_id' => $chat_id,
+                'user_id' => $user_id,
+                'last_activity' => $last_activity
+            ), 'id', $id);
+            $QueryResults = $this->spamProtection->getDatabase("IVDatabase")->query($Query);
+
+            if($QueryResults == true)
+            {
+                return true;
+            }
+            else
+            {
+                throw new DatabaseException($Query, $this->spamProtection->getDatabase("IVDatabase")->error);
+            }
+        }
     }
