bce8ef4dadc085acc51b028667284eeb0ff4241b
netkas <netkas@intellivoid.info>
2020-06-10 00:43:10 -0400

    Added MessageHashNotFoundException to \SpamProtection\Exceptions

--- src/SpamProtection/Managers/MessageLogManager.php
+++ src/SpamProtection/Managers/MessageLogManager.php
@@ -4,9 +4,14 @@
     namespace SpamProtection\Managers;
 
 
+    use msqg\QueryBuilder;
+    use SpamProtection\Exceptions\DatabaseException;
+    use SpamProtection\Exceptions\UnsupportedMessageException;
     use SpamProtection\Objects\MessageLog;
     use SpamProtection\Objects\TelegramObjects\Message;
     use SpamProtection\SpamProtection;
+    use SpamProtection\Utilities\Hashing;
+    use ZiProto\ZiProto;
 
     /**
      * Class MessageLogManager
@@ -28,8 +33,120 @@
             $this->spamProtection = $spamProtection;
         }
 
+        /**
+         * @param Message $message
+         * @param float $spam_prediction
+         * @param float $ham_prediction
+         * @return MessageLog
+         * @throws UnsupportedMessageException
+         */
         public function registerMessage(Message $message, float $spam_prediction, float $ham_prediction): MessageLog
         {
-            if($message)
+            if($message->Chat == null)
+            {
+                throw new UnsupportedMessageException("The message is missing the 'Chat' object");
+            }
+
+            if($message->From == null)
+            {
+                throw new UnsupportedMessageException("The message is missing the 'User' object");
+            }
+
+            if($message->getText() == null)
+            {
+                throw new UnsupportedMessageException();
+            }
+
+            $message_id = (int)$message->MessageID;
+            $chat_id = (int)$message->Chat->ID;
+            $chat = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode($message->Chat->toArray()));
+            $user_id = (int)$message->From->ID;
+            $user = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode($message->From->toArray()));
+            $forward_from = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode(array()));
+            $forward_from_chat = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode(array()));
+            $forward_from_message_id = (int)0;
+
+            if($message->ForwardFrom !== null)
+            {
+                $forward_from = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode(
+                    $message->ForwardFrom->toArray()
+                ));
+            }
+
+            if($message->ForwardFromChat !== null)
+            {
+                $forward_from_chat = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(ZiProto::encode(
+                    $message->ForwardFromChat->toArray()
+                ));
+            }
+
+            if($message->ForwardFromMessageID !== null)
+            {
+                $forward_from_message_id = (int)$message->ForwardFromMessageID;
+            }
+
+            $timestamp = (int)time();
+            $content_hash = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string(Hashing::messageContent($message->getText()));
+            $message_hash = Hashing::messageHash($message_id, $chat_id, $user_id, $timestamp, $content_hash);
+            $spam_prediction = (float)$spam_prediction;
+            $ham_prediction = (float)$ham_prediction;
+
+            $Query = QueryBuilder::insert_into('message_logs', array(
+                'message_hash' => $message_hash,
+                'message_id' => $message_id,
+                'chat_id' => $chat_id,
+                'chat' => $chat,
+                'user_id' => $user_id,
+                'user' => $user,
+                'forward_from' => $forward_from,
+                'forward_from_chat' => $forward_from_chat,
+                'forward_from_message_id' => $forward_from_message_id,
+                'content_hash' => $content_hash,
+                'spam_prediction' => $spam_prediction,
+                'ham_prediction' => $ham_prediction,
+                'timestamp' => $timestamp
+            ));
+        }
+
+        public function getMessage(string $message_hash): MessageLog
+        {
+            $message_hash = $this->spamProtection->getDatabase("MainDatabase")->real_escape_string($message_hash);
+            
+            $Query = QueryBuilder::select('message_logs', array(
+                'id',
+                'message_hash',
+                'message_id',
+                'chat_id',
+                'chat',
+                'user_id',
+                'user',
+                'forward_from',
+                'forward_from_chat',
+                'forward_from_message_id',
+                'content_hash',
+                'spam_prediction',
+                'ham_prediction',
+                'timestamp'
+            ), 'message_id', $message_hash);
+
+            $QueryResults = $this->spamProtection->getDatabase("MainDatabase")->query($Query);
+
+            if($QueryResults == false)
+            {
+                throw new DatabaseException($Query, $this->spamProtection->getDatabase("MainDatabase")->error);
+            }
+            else
+            {
+                if($QueryResults->num_rows !== 1)
+                {
+                    throw new ();
+                }
+
+                $Row = $QueryResults->fetch_array(MYSQLI_ASSOC);
+                $Row['user'] = ZiProto::decode($Row['user']);
+                $Row['chat'] = ZiProto::decode($Row['chat']);
+                $Row['session_data'] = ZiProto::decode($Row['session_data']);
+                return TelegramClient::fromArray($Row);
+            }
         }
     }
