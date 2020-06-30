aa823ede5d49922c61fa3e2459c6c9292d5d96c9
netkas <netkas@intellivoid.info>
2020-06-29 23:40:30 -0400

    Added ImageHash to \SpamProtectionObjects

--- database/image_logs.sql
+++ database/image_logs.sql
@@ -3,8 +3,8 @@ create table if not exists spam_protection.image_logs
     id                      int(255) auto_increment comment 'The unique internal database ID of this record',
     message_hash            varchar(256) null comment 'The hash of the message',
     message_id              int(255)     null comment 'The ID of the message sent in the chat',
-    chat_id                 int(255)     null comment 'The ID of the chat this image was sent in',
     photo_size              blob         null comment 'ZiProto Encoded data of the photo size',
+    chat_id                 int(255)     null comment 'The ID of the chat this image was sent in',
     chat                    blob         null comment 'ZiProto Encoded data of the chat',
     user_id                 int(255)     null comment 'The user ID of who sent this image',
     user                    blob         null comment 'ZiProto Encoded data of the user',
@@ -15,9 +15,11 @@ create table if not exists spam_protection.image_logs
     spam_prediction         float        null comment 'The spam prediction of the image',
     ham_prediction          float        null comment 'The ham prediction of the image',
     timestamp               int(255)     null comment 'The Unix Timestamp of when this record was created',
-    constraint image_logs_id_uindex unique (id),
     constraint image_logs_content_hash_uindex unique (content_hash),
-    constraint image_logs_message_hash_uindex unique (message_hash)
+    constraint image_logs_id_uindex unique (id),
+    constraint image_logs_message_hash_uindex unique (message_hash),
+    constraint image_logs_message_id_chat_id_uindex unique (message_id, chat_id),
+    constraint image_logs_message_id_chat_id_user_id_uindex unique (message_id, chat_id, user_id)
 ) comment 'Table of image hashes';
 
 alter table spam_protection.image_logs add primary key (id);
--- src/SpamProtection/Objects/ImageLog.php
+++ src/SpamProtection/Objects/ImageLog.php
@@ -3,7 +3,7 @@
 
     namespace SpamProtection\Objects;
 
-
+    use SpamProtection\Objects\TelegramObjects\PhotoSize;
     use TelegramClientManager\Objects\TelegramClient\Chat;
     use TelegramClientManager\Objects\TelegramClient\User;
 
@@ -35,26 +35,11 @@
         public $MessageID;
 
         /**
-         * Identifier for this file, which can be used to download or reuse the file
+         * PhotoSize (File Information)
          *
-         * @var string
+         * @var PhotoSize
          */
-        public $FileID;
-
-        /**
-         * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-         * Can't be used to download or reuse the file.
-         *
-         * @var string
-         */
-        public $FileUniqueID;
-
-        /**
-         * The size of the file
-         *
-         * @var int
-         */
-        public $FileSize;
+        public $PhotoSize;
 
         /**
          * The ID of the chat that this message was sent in
@@ -113,20 +98,6 @@
         public $ContentHash;
 
         /**
-         * The width of the image
-         *
-         * @var int
-         */
-        public $Width;
-
-        /**
-         * The height of the image
-         *
-         * @var int
-         */
-        public $Height;
-
-        /**
          * The spam prediction value of the image
          *
          * @var float|int
@@ -146,4 +117,119 @@
          * @var int
          */
         public $Timestamp;
+
+        /**
+         * Returns an array which represents this object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return array(
+                'id' => (int)$this->ID,
+                'message_hash' => $this->MessageHash,
+                'message_id' => (int)$this->MessageID,
+                'photo_size' => $this->PhotoSize->toArray(),
+                'chat_id' => (int)$this->ChatID,
+                'chat' => $this->Chat->toArray(),
+                'user_id' => (int)$this->UserID,
+                'user' => $this->User->toArray(),
+                'forward_from' => $this->ForwardForm->toArray(),
+                'forward_from_chat' => $this->ForwardFromChat->toArray(),
+                'forward_from_message_id' => $this->ForwardFromMessageID,
+                'content_hash' => $this->ContentHash,
+                'spam_prediction' => (float)$this->SpamPrediction,
+                'ham_prediction' => (float)$this->HamPrediction,
+                'timestamp' => (int)$this->Timestamp
+            );
+        }
+
+        /**
+         * Constructs object from array
+         *
+         * @param array $data
+         * @return ImageLog
+         * @noinspection DuplicatedCode
+         */
+        public static function fromArray(array $data): ImageLog
+        {
+            $ImageLogObject = new ImageLog();
+
+            if(isset($data['id']))
+            {
+                $ImageLogObject->ID = (int)$data['id'];
+            }
+
+            if(isset($data['message_hash']))
+            {
+                $ImageLogObject->MessageHash = $data['message_hash'];
+            }
+
+            if(isset($data['message_id']))
+            {
+                $ImageLogObject->MessageID = (int)$data['message_id'];
+            }
+
+            if(isset($data['photo_size']))
+            {
+                $ImageLogObject->PhotoSize = PhotoSize::fromArray($data['photo_size']);
+            }
+
+            if(isset($data['chat_id']))
+            {
+                $ImageLogObject->ChatID = (int)$data['chat_id'];
+            }
+
+            if(isset($data['chat']))
+            {
+                $ImageLogObject->Chat = Chat::fromArray($data['chat']);
+            }
+
+            if(isset($data['user_id']))
+            {
+                $ImageLogObject->UserID = (int)$data['user_id'];
+            }
+
+            if(isset($data['user']))
+            {
+                $ImageLogObject->User = User::fromArray($data['user']);
+            }
+
+            if(isset($data['forward_from']))
+            {
+                $ImageLogObject->ForwardForm = User::fromArray($data['forward_from']);
+            }
+
+            if(isset($data['forward_from_chat']))
+            {
+                $ImageLogObject->ForwardFromChat = Chat::fromArray($data['forward_from_chat']);
+            }
+
+            if(isset($data['forward_from_message_id']))
+            {
+                $ImageLogObject->ForwardFromMessageID = (int)$data['forward_from_message_id'];
+            }
+
+            if(isset($data['content_hash']))
+            {
+                $ImageLogObject->ContentHash = $data['content_hash'];
+            }
+
+            if(isset($data['spam_prediction']))
+            {
+                $ImageLogObject->SpamPrediction = (float)$data['spam_prediction'];
+            }
+
+            if(isset($data['ham_prediction']))
+            {
+                $ImageLogObject->HamPrediction = (float)$data['ham_prediction'];
+            }
+
+            if(isset($data['timestamp']))
+            {
+                $ImageLogObject->Timestamp = (int)$data['timestamp'];
+            }
+
+            return $ImageLogObject;
+        }
     }
