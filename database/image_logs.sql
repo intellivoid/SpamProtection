8065f2073e30d3d74f6c25865279a2c51ae0b090
netkas <netkas@intellivoid.info>
2020-06-29 23:12:46 -0400

    Updated ImageHash structure

--- database/image_logs.sql
+++ database/image_logs.sql
@@ -3,10 +3,8 @@ create table if not exists spam_protection.image_logs
     id                      int(255) auto_increment comment 'The unique internal database ID of this record',
     message_hash            varchar(256) null comment 'The hash of the message',
     message_id              int(255)     null comment 'The ID of the message sent in the chat',
-    file_id                 varchar(256) null comment 'Identifier for this file, which can be used to download or reuse the file',
-    file_unique_id          varchar(256) null comment 'Unique identifier for this file, which is supposed to be the same over time and for different bots. Can''t be used to download or reuse the file.',
-    file_size               int(255)     null comment 'Optional. File size',
     chat_id                 int(255)     null comment 'The ID of the chat this image was sent in',
+    photo_size              blob         null comment 'ZiProto Encoded data of the photo size',
     chat                    blob         null comment 'ZiProto Encoded data of the chat',
     user_id                 int(255)     null comment 'The user ID of who sent this image',
     user                    blob         null comment 'ZiProto Encoded data of the user',
@@ -14,16 +12,12 @@ create table if not exists spam_protection.image_logs
     forward_from_chat       blob         null comment 'ZiProto Encoded data of the chat (channel) that this image is originally from',
     forward_from_message_id int(255)     null comment 'The original message ID',
     content_hash            varchar(256) null comment 'The SHA256 hash of the image contents',
-    width                   int(255)     null comment 'Photo width',
-    height                  int(255)     null comment 'Photo height',
     spam_prediction         float        null comment 'The spam prediction of the image',
     ham_prediction          float        null comment 'The ham prediction of the image',
     timestamp               int(255)     null comment 'The Unix Timestamp of when this record was created',
-    constraint image_logs_file_unique_id_uindex unique (file_unique_id),
     constraint image_logs_id_uindex unique (id),
     constraint image_logs_content_hash_uindex unique (content_hash),
     constraint image_logs_message_hash_uindex unique (message_hash)
 ) comment 'Table of image hashes';
 
 alter table spam_protection.image_logs add primary key (id);
-
